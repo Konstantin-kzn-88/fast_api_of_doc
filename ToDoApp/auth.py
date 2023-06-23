1e726df861e3d0fb1cc830fd09a47d48582af02c
User <kuznetsovkm@yandex.ru>
2023-06-23 20:46:23 +0300

    created user + hash password+db

--- ToDoApp/auth.py
+++ ToDoApp/auth.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from typing import Optional
 import models
 from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
 
 
-class CraeteUser(BaseModel):
+
+class CreateUser(BaseModel):
     username: str
     email: Optional[str]
     first_name: str
@@ -15,13 +18,25 @@ class CraeteUser(BaseModel):
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
+models.Base.metadata.create_all(bind=engine)
+
+
 app = FastAPI()
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
 
 def get_password_hash(password: object) -> object:
     return bcrypt_context.hash(password)
 
 @app.post('/create/user')
-async def create_new_user(create_user: CraeteUser):
+async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
     create_user_model = models.Users()
     create_user_model.email = create_user.email
     create_user_model.username = create_user.username
@@ -33,5 +48,13 @@ async def create_new_user(create_user: CraeteUser):
     create_user_model.hahed_password = hash_password
     create_user_model.is_active = True
 
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
+    return successful_response(201)
+
+def successful_response(status_code:int):
+    return {
+        'status': status_code,
+        'transaction': 'successful'
+    }
 
