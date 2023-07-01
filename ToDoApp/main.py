3374c0ccd6ccc72c573e49407891f7bd32cb5564
User <kuznetsovkm@yandex.ru>
2023-07-01 13:14:05 +0300

    Routers и api users

--- ToDoApp/main.py
+++ ToDoApp/main.py
@@ -1,119 +1,23 @@
-from typing import Optional
 
-from fastapi import FastAPI, Depends, HTTPException
+
+from fastapi import FastAPI, Depends
 import models
-from database import engine, SessionLocal
-from sqlalchemy.orm import Session
-from pydantic import BaseModel, Field
+from database import engine
 
-from auth import get_current_user, get_user_exception
+from routers import auth, todos, users
+from company import companyapis, dependencies
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-
-def get_db():
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
-
-
-class ToDo(BaseModel):
-    title: str
-    description: Optional[str]
-    priority: int = Field(gt=0, lt=6, description='Must be between 1-5')
-    complete: bool
-
-
-@app.get('/')
-async def read_all(db: Session = Depends(get_db)):
-    return db.query(models.Todos).all()
-
-
-@app.get('/todos/user')
-async def read_all_by_user(user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
-    if user is None:
-        raise get_user_exception()
-    return db.query(models.Todos).filter(models.Todos.owner_id == user.get('user_id')).all()
-
-
-@app.get('/todo/{todo_id}')
-async def read_todo(todo_id: int, user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
-    if user is None:
-        raise get_user_exception()
-    todo_model = db.query(models.Todos). \
-        filter(models.Todos.id == todo_id). \
-        filter(models.Todos.owner_id == user.get('user_id')) \
-        .first()
-    if todo_model is not None:
-        return todo_model
-    raise http_exception()
-
-
-@app.post('/')
-async def create_todo(todo: ToDo, user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
-    if user is None:
-        raise get_user_exception()
-    todo_model = models.Todos()
-    todo_model.title = todo.title
-    todo_model.description = todo.description
-    todo_model.priority = todo.priority
-    todo_model.complete = todo.complete
-    todo_model.owner_id = user.get('user_id')
-
-    db.add(todo_model)
-    db.commit()
-
-    return successful_response(201)
-
-
-@app.put('/{todo_id}')
-async def update_todo(todo_id: int, todo: ToDo, user: dict = Depends(get_current_user),  db: Session = Depends(get_db)):
-
-    if user is None:
-        raise get_user_exception()
-
-    todo_model = db.query(models.Todos).\
-        filter(models.Todos.id == todo_id).\
-        filter(models.Todos.owner_id == user.get('user_id'))\
-        .first()
-    if todo_model is None:
-        raise http_exception()
-    todo_model.title = todo.title
-    todo_model.description = todo.description
-    todo_model.priority = todo.priority
-    todo_model.complete = todo.complete
-
-    db.add(todo_model)
-    db.commit()
-
-    return successful_response(200)
-
-
-@app.delete('/{todo_id}')
-async def delete_todo(todo_id: int, user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
-    if user is None:
-        raise get_user_exception()
-    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
-    if todo_model is None:
-        raise http_exception()
-    db.query(models.Todos).filter(models.Todos.id == todo_id).\
-        filter(models.Todos.owner_id == user.get('user_id')).\
-        delete()
-    db.commit()
-
-    return successful_response(200)
-
-
-def http_exception():
-    return HTTPException(status_code=404, detail="Todo not found")
-
-
-def successful_response(status_code: int):
-    return {
-        'status': status_code,
-        'transaction': 'successful'
-    }
+app.include_router(auth.router)
+app.include_router(todos.router)
+app.include_router(users.router)
+app.include_router(
+    companyapis.router,
+    prefix='/companyapis',
+    tags=['companyapis'],
+    dependencies=[Depends(dependencies.get_token_header)],
+    responses={418: {'description': 'Internal use only'}}
+)
---
+++ ToDoApp/routers/users.py
@@ -0,0 +1,86 @@
+import sys
+
+sys.path.append('..')
+
+from fastapi import Depends, APIRouter
+import models
+from database import SessionLocal, engine
+from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
+from .auth import get_current_user, get_user_exception, verify_password, get_password_hash
+
+router = APIRouter(
+    prefix='/users',
+    tags=['users'],
+    responses={404: {'descrition': 'Not found'}}
+
+)
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+class UserVerification(BaseModel):
+    user_name: str
+    password: str
+    new_password: str
+
+
+@router.get('/')
+async def read_all_users(db: Session = Depends(get_db)):
+    return db.query(models.Users).all()
+
+
+@router.get('/user/{todo_id}')
+async def user_by_path(user_id: int, db: Session = Depends(get_db)):
+    user_model = db.query(models.Users).filter(models.Users.id == user_id).all()
+    print(user_model)
+    if user_model is not None:
+        return user_model
+    return 'Invalid user_id'
+
+
+@router.get('/user/')
+async def user_by_path(user_id: int, db: Session = Depends(get_db)):
+    user_model = db.query(models.Users).filter(models.Users.id == user_id).first()
+    if user_model is not None:
+        return user_model
+    return 'Invalid user_id'
+
+
+@router.put('/user/password')
+async def user_pass_word_change(user_verification: UserVerification,
+                                user: dict = Depends(get_current_user),
+                                                     db: Session = Depends(get_db)):
+    if user is None:
+        raise get_user_exception()
+    user_model = db.query(models.Users).filter(models.Users.id == user.get('user_id')).first()
+    if user_model is not None:
+        if user_verification.user_name == user_model.username and verify_password(user_verification.password, user_model.hashed_password):
+            user_model.hashed_password = get_password_hash(user_verification.new_password)
+            db.add(user_model)
+            db.commit()
+            return 'Successful change password'
+
+    return 'Invalid user or request'
+
+
+@router.delete('/user')
+async def delete_user(user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
+    if user is None:
+        raise get_user_exception()
+    user_model = db.query(models.Users).filter(models.Users.id == user.get('user_id')).first()
+    if user_model is None:
+        return 'Invalid user or request'
+    db.query(models.Users).filter(models.Users.id == user.get('user_id')).delete()
+    db.commit()
+    return 'Delete succssesful'
+
+
