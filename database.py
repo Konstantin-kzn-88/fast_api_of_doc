abef85089d872081c818f5237e567220a2f7a793
User <kuznetsovkm@yandex.ru>
2023-07-02 13:06:22 +0300

    Переделал на MySQL Добавил Alembic (Git для баз данных) Добавил таблицу Address, отредактировал таблицу Users

--- database.py
+++ database.py
@@ -2,15 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://u1082920_test_to:1501kZn1501!@server167.hosting.reg.ru/u1082920_fast_api_todo'
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={'check_same_thread': False}
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
