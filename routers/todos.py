76ac1f3c885b4f81d294b3b06ea733d8574215f4
User <kuznetsovkm@yandex.ru>
2023-07-04 23:31:19 +0300

    HTML + post в MySQL (todos table)

--- routers/todos.py
+++ routers/todos.py
@@ -1,8 +1,12 @@
 import sys
+
 sys.path.append('..')
 
+from starlette import status
+from starlette.responses import RedirectResponse
+
 from typing import Optional
-from fastapi import Request, Depends, HTTPException, APIRouter
+from fastapi import Form, Request, Depends, HTTPException, APIRouter
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -16,12 +20,13 @@ from fastapi.templating import Jinja2Templates
 router = APIRouter(
     prefix='/todos',
     tags=['todos'],
-    responses={404:{'description':'Not found'}}
+    responses={404: {'description': 'Not found'}}
 
 )
 
 models.Base.metadata.create_all(bind=engine)
 templates = Jinja2Templates(directory='templates/')
+
 
 def get_db():
     try:
@@ -34,17 +39,36 @@ def get_db():
 @router.get('/', response_class=HTMLResponse)
 async def read_all_by_user(request: Request, db: Session = Depends(get_db)):
     todos = db.query(models.Todos).filter(models.Todos.owner_id == 1).all()
-    return templates.TemplateResponse('home.html', {'request': request, 'todos': todos} )
+    return templates.TemplateResponse('home.html', {'request': request, 'todos': todos})
+
 
 @router.get('/add-todo', response_class=HTMLResponse)
 async def add_new_todo(request: Request):
-    return templates.TemplateResponse('add_todo.html', {'request': request} )
+    return templates.TemplateResponse('add_todo.html', {'request': request})
+
+
+@router.post('/add-todo', response_class=HTMLResponse)
+async def create_todo(request: Request, title: str = Form(...),
+                      description: str = Form(...),
+                      priority: str = Form(...),
+                      db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+    todo_model.complete = False
+    todo_model.owner_id = 1
+
+    db.add(todo_model)
+    db.commit()
+
+    return RedirectResponse(url='/todos', status_code=status.HTTP_302_FOUND)
+
+
 
 @router.get('/edit-todo/{todo_id}', response_class=HTMLResponse)
 async def edit_todo(request: Request):
-    return templates.TemplateResponse('edit_todo.html', {'request': request} )
-
-
+    return templates.TemplateResponse('edit_todo.html', {'request': request})
 
 # API без HTML
 # class ToDo(BaseModel):
