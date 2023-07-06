ffa45a3e63deda286c6d8cd247e7078b65ef42ca
User <kuznetsovkm@yandex.ru>
2023-07-06 22:14:47 +0300

    HTML + logout

--- routers/auth.py
+++ routers/auth.py
@@ -169,6 +169,14 @@ async def login(request: Request, db: Session = Depends(get_db)):
         return templates.TemplateResponse('login.html', {'request': request, 'msg': msg})
 
 
+@router.get('/logout')
+async def logout(request: Request):
+    msg = 'Logout Successful'
+    response = templates.TemplateResponse('login.html', {'request': request, 'msg':msg})
+    response.delete_cookie(key='access_token')
+    return response
+
+
 @router.get('/register', response_class=HTMLResponse)
 async def registration_page(request: Request):
     return templates.TemplateResponse('register.html', {'request': request})
